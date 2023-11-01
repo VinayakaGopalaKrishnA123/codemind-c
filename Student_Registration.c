cb8a7e679b73f9e6fa06312d4b51ca469d3fc597
VinayakaGopalaKrishnA123 <147472515+VinayakaGopalaKrishnA123@users.noreply.github.com>
2023-11-01 19:35:57 +0530

    #include<stdio.h> int main(){     int a,b,c,d,e,f,g;     scanf("%d%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f,&g);     if (c>=b+d) printf("YES ");     else printf("NO ");     if (f>=e+g) printf("YES");     else printf("NO"); }

---
+++ Student_Registration.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b,c,d,e,f,g;
+    scanf("%d%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f,&g);
+    if (c>=b+d) printf("YES
+");
+    else printf("NO
+");
+    if (f>=e+g) printf("YES");
+    else printf("NO");
+}
