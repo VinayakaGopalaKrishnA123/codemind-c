99521a8d4afede78b34bc8c9adfe2b9fac52c2a9
VinayakaGopalaKrishnA123 <147472515+VinayakaGopalaKrishnA123@users.noreply.github.com>
2023-11-19 15:59:55 +0530

    #include<stdio.h> int main(){ 	int n,i,k,cnt; 	scanf("%d",&n); 	int tot=0; 	for(i=1;i<=n;i++){ 		k=0; 		cnt=0; 		for(k=1;k<=n;k++){ 			if(i%k==0){ 				cnt++; 			} 		} 		if(cnt==9){ 			tot++; 			printf("%d ",i); 		} 	} 	printf(" Total=%d",tot); }

--- Program_to_find_the_number_of_integers_with_exactly_9_divisors.c
+++ Program_to_find_the_number_of_integers_with_exactly_9_divisors.c
@@ -1,19 +1,21 @@
 #include<stdio.h>
 int main(){
-    int a;
-    scanf("%d",&a);
-    int i,k,cnt,j=0;
-    for(i=1;i<=a;i++){
-    	cnt=0;
-        for(k=1;k<=i;k++){
-            if(i%k==0) cnt++;
-        }
-        if(cnt==9) {
-            j++; 
-            printf("%d ",i);
-        }
-        
-    }
-    printf("
-Total=%d",j);
+	int n,i,k,cnt;
+	scanf("%d",&n);
+	int tot=0;
+	for(i=1;i<=n;i++){
+		k=0;
+		cnt=0;
+		for(k=1;k<=n;k++){
+			if(i%k==0){
+				cnt++;
+			}
+		}
+		if(cnt==9){
+			tot++;
+			printf("%d ",i);
+		}
+	}
+	printf("
+Total=%d",tot);
 }
