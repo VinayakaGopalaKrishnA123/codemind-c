6d4016aa1b8117423e9f64b695530bbed19e58bb
VinayakaGopalaKrishnA123 <147472515+VinayakaGopalaKrishnA123@users.noreply.github.com>
2023-11-22 12:00:23 +0530

    #include<stdio.h> int main(){     int n,i,k;     scanf("%d",&n);     for(i=1;i<=n;i++){         for(k=1;k<=i;k++){             printf("%d ",i);         }         printf(" ");     } }

---
+++ Basic_Pattern_2.2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n,i,k;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(k=1;k<=i;k++){
+            printf("%d ",i);
+        }
+        printf("
+");
+    }
+}
+    
