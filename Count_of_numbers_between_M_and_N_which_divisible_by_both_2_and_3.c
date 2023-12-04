25a5ee36cdbe3fd2ea6c37484efe0be71022c571
VinayakaGopalaKrishnA123 <147472515+VinayakaGopalaKrishnA123@users.noreply.github.com>
2023-12-04 21:55:43 +0530

    #include<stdio.h> int main(){     int a,b,i;     scanf("%d %d",&a,&b);     int cnt=0;     for(i=a;i<=b;i++){         if(i%2==0 && i%3==0){             cnt++;         }     }     printf("%d",cnt); }

---
+++ Count_of_numbers_between_M_and_N_which_divisible_by_both_2_and_3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a,b,i;
+    scanf("%d %d",&a,&b);
+    int cnt=0;
+    for(i=a;i<=b;i++){
+        if(i%2==0 && i%3==0){
+            cnt++;
+        }
+    }
+    printf("%d",cnt);
+}
