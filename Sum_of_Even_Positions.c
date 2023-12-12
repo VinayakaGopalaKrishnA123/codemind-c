07cc7fa34fbe3c03856e904b6cd374dee6ad8c33
VinayakaGopalaKrishnA123 <147472515+VinayakaGopalaKrishnA123@users.noreply.github.com>
2023-12-12 20:38:43 +0530

    #include<stdio.h> int main(){     int n;     int i;     scanf("%d",&n);     int b[n],sum=0;     for(i=0;i<=n-1;i++){         scanf("%d",&b[i]);         if(i%2==0) sum=sum+b[i];     }     printf("%d",sum);
    }

---
+++ Sum_of_Even_Positions.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n;
+    int i;
+    scanf("%d",&n);
+    int b[n],sum=0;
+    for(i=0;i<=n-1;i++){
+        scanf("%d",&b[i]);
+        if(i%2==0) sum=sum+b[i];
+    }
+    printf("%d",sum);
+    
+}
